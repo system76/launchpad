6eb054536306c5f2f7535016dbfc53eb9e02d2cc
Ian Santopietro <isantop@gmail.com>
2022-06-10 10:17:25 -0600

    feat(selma): add basic test

--- src/launchpad/data.py
+++ src/launchpad/data.py
@@ -24,4 +24,29 @@ SOFTWARE.
 
 """
 
-launchpad_version = '0.1a1'
+launch_lite_testing_code = """
+((10\\1)
+(T2  D=3.175 CR=0 - ZMIN=4.182 - flat end mill)
+G90 G94
+G17
+G21
+
+S10000 M3
+(2D Contour8)
+G54
+G1 X-197. Y0. F5000.
+G91
+Y-307. F1000.
+Y307.
+X38.
+Y-307.
+Y307.
+X38.
+Y-307.
+Y307.
+G90
+X-197. 
+M5
+
+M30
+"""
